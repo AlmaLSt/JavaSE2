0a43727e17e2901dea43eb8c758e7fcd60944040
AlmaLst <alma.loverasotelo@gmail.com>
2023-04-06 00:03:59 -0600

    Reto 2-Partido model and repository

---
+++ s1/src/main/java/org/bedu/javase2/ejemplo/ejemplo1/s1/models/Partido.java
@@ -0,0 +1,2 @@
+package org.bedu.javase2.ejemplo.ejemplo1.s1.models;public class Partido {
+}
---
+++ s1/src/main/java/org/bedu/javase2/ejemplo/ejemplo1/s1/repositories/PartidoRepository.java
@@ -0,0 +1,2 @@
+package org.bedu.javase2.ejemplo.ejemplo1.s1.repositories;public interface PartidoRepository {
+}
