4783848868e26d18e2e0255048f5e6915062ce64
AlmaLst <alma.loverasotelo@gmail.com>
2023-04-27 23:12:27 -0600

    Ejercicio y reto 2-Empleado

---
+++ sesion7/src/main/java/org/bedu/javase2/sesion7/entity/Empleado.java
@@ -0,0 +1,15 @@
+package org.bedu.javase2.sesion7.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Empleado {
+
+    private String id;
+    private String nombre;
+
+}
