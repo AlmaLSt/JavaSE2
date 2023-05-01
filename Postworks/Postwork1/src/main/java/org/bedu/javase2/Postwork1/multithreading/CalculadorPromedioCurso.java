763575f9d95db99b8cfe979281a8ee64650f3b4a
AlmaLst <alma.loverasotelo@gmail.com>
2023-04-30 23:27:24 -0600

    Postwork 2

--- Postworks/Postwork1/src/main/java/org/bedu/javase2/Postwork1/multithreading/CalculadorPromedioCurso.java
+++ Postworks/Postwork1/src/main/java/org/bedu/javase2/Postwork1/multithreading/CalculadorPromedioCurso.java
@@ -1,5 +1,24 @@
 package org.bedu.javase2.Postwork1.multithreading;
 
-public class CalculadorPromedioCurso {
+import org.bedu.javase2.Postwork1.model.Curso;
 
+public class CalculadorPromedioCurso implements Runnable{
+    private Curso curso;
+    private double promedio;
+
+    public CalculadorPromedioCurso(Curso curso) {
+        this.curso = curso;
+    }
+
+    @Override
+    public void run() {
+        int Alumnos = 0;
+        for(Integer i : curso.getCalificaciones().values()){
+            promedio += i;
+            Alumnos ++;
+        }
+        promedio /= Alumnos;
+
+        System.out.println("Promedio del curso: " + curso.getId() + " " + curso.getMateria().getNombre() + " = " + promedio);
+    }
 }
