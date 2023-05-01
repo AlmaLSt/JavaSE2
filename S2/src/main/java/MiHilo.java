f3cce7bb9399230c9491def54598684795a10086
AlmaLst <alma.loverasotelo@gmail.com>
2023-04-30 18:23:17 -0600

    Ejemplo 1

--- S2/src/main/java/MiHilo.java
+++ S2/src/main/java/MiHilo.java
@@ -1,2 +1,46 @@
-package PACKAGE_NAME;public class MiHilo {
+import java.util.concurrent.TimeUnit;
+
+public class MiHilo extends Thread{
+    String parametro;
+
+
+    public void setParametro(String parametro){
+        this.parametro = parametro;
+    }
+    public MiHilo(String parametro){
+        this.parametro = parametro;
+    }
+    @Override
+    public void run() {
+        while(!"terminar".equals(parametro)){
+            mostrarInformacion();
+            pausarUnSegundo();
+        }
+        mostrarInformacion();
+    }
+    private void mostrarInformacion(){
+        String tipoHilo = isDaemon() ? "daemon" : "usuario";
+        System.out.println(tipoHilo + "\t| Nombre: " + getName() + "\t| ID: " + getId() + "\t| Parametro: " + parametro);
+    }
+
+    public static void pausarUnSegundo() {
+        try {
+            TimeUnit.SECONDS.sleep(1);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        MiHilo hilo1 = new MiHilo("1");
+        hilo1.start();
+
+        MiHilo hilo2 = new MiHilo("2");
+        hilo2.setDaemon(true);
+        hilo2.start();
+
+        MiHilo.pausarUnSegundo();
+        hilo1.setParametro("terminar");
+        System.out.println("**FINAL**");
+    }
 }
