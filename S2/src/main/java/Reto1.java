f31a9f3ca07096ed9d1a13fba3a55496ed525c5b
AlmaLst <alma.loverasotelo@gmail.com>
2023-04-30 20:32:01 -0600

    Reto 1

--- S2/src/main/java/Reto1.java
+++ S2/src/main/java/Reto1.java
@@ -1,2 +1,41 @@
-package PACKAGE_NAME;public class Reto1 {
+import java.util.concurrent.TimeUnit;
+
+public class Reto1 extends Thread {
+    @Override
+    public void run() {
+        for (int i = 0; i <= 10; i++) {
+            System.out.println("Hilo1: " + i);
+            try {
+                TimeUnit.SECONDS.sleep(1);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public static class Hilo2 extends Thread {
+
+        @Override
+        public void run() {
+            while(true){
+                System.out.println("Hilo2: Sigo en ejecución...");
+                try {
+                    TimeUnit.MILLISECONDS.sleep(500);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+
+        Reto1 hilo1 = new Reto1();
+        hilo1.start();
+
+        Hilo2 hilo2= new Hilo2();
+        hilo2.setDaemon(true);
+        hilo2.start();
+    }
+
 }
